33808a101c96ea31ccbfd980c18aaf6c5eb9b384
Pieter Venter <pieterven12@gmail.com>
2022-09-21 19:44:51 +0200

    Project Init

---
+++ UniHealth/UniHealth/UniHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniHealthApp.swift
+//  UniHealth
+//
+//  Created by Pieter Venter on 2022/09/21.
+//
+
+import SwiftUI
+
+@main
+struct UniHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
